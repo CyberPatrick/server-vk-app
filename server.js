0107673d4d7cfcb29c0565fff24015b2280cd116
CyberPatrick <57152405+CyberPatrick@users.noreply.github.com>
2020-08-23 19:40:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
-
-const server = new WebSocket.Server({ port: 3001 });
+const port = process.env.PORT || 5000;
+const server = new WebSocket.Server({ port: [port] });
 
 let games = {};
 
