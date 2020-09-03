c60b66a02396c1e8ca99ca0c0e1d312171b8fbfb
CyberPatrick <57152405+CyberPatrick@users.noreply.github.com>
2020-09-03 20:42:19 +0300

    Some fix

--- server.js
+++ server.js
@@ -45,9 +45,13 @@ const player_1_symbol = Symbol('player_1');
 
 
 async function checkUser(user_id, first_name, last_name) {
-    const answer = await conn.execute('SELECT user_id, games, wins, points FROM `tic-tac-toe` WHERE user_id=?', [user_id]);
+    const answer;
+    conn.execute('SELECT user_id, games, wins, points FROM `tic-tac-toe` WHERE user_id=?', [user_id])
+        .then(result => {
+            answer = result;
+        })
     if (!answer) {
-        await conn.execute('INSERT INTO `tic-tac-toe` (user_id, first_name, last_name) VALUES (?)', [user_id, first_name, last_name]);
+        conn.execute('INSERT INTO `tic-tac-toe` (user_id, first_name, last_name) VALUES (?)', [user_id, first_name, last_name]);
         return {games: 0, wins: 0, points: 0};
     }
     return {games: answer[1], wins: answer[2], points: answer[3]};
